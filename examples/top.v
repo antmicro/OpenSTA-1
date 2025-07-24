4ecad5097efaef78be9bf4583c4b130f49bf1042
Mateusz Gancarz <mgancarz@antmicro.com>
2025-07-24 08:40:53 +0200

    [#80124] update example files for timing paths

--- examples/top.v
+++ examples/top.v
@@ -4,10 +4,10 @@ module top (in1, in2, in3, clk1, clk2, clk3, out1, out2);
   output out1, out2;
   wire w1, w2, w3;
 
-  BUF_X1 b1 (.A(in1), .Z(w1));
-  BUF_X1 b2 (.A(in2), .Z(w2));
-  BUF_X1 b3 (.A(in3), .Z(w3));
+  timing_test tt1(.in1(in1), .in2(in2), .in3(in3), .clk1(clk1), .clk2(clk2), .clk3(clk3), .out1(w1), .out2(w2));
 
-  timing_test tt0(.in1(w1), .in2(w2), .in3(w3), .clk1(clk1), .clk2(clk2), .clk3(clk3), .out1(out1), .out2(out2));
+  BUF_X1 b1 (.A(w1), .Z(w3));
+
+  timing_test tt2(.in1(w3), .in2(w2), .in3(in3), .clk1(clk1), .clk2(clk2), .clk3(clk3), .out1(out1), .out2(out2));
 
 endmodule // top
