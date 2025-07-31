a237eff6e6d361bb592b656e4823b72236e78bcd
Mateusz Gancarz <mgancarz@antmicro.com>
2025-07-31 12:44:50 +0200

    [#80367] define clock in exported_timing_model examples

--- examples/exported_timing_model1.tcl
+++ examples/exported_timing_model1.tcl
@@ -1,4 +1,6 @@
 read_liberty nangate45_slow.lib.gz
 read_verilog exported_timing_model.v
 link_design top
+create_clock -name clk -period 10 {clk}
+set_input_delay -clock clk 0 {in}
 write_timing_model exported_timing_model1.lib -instance [sta::find_instance i1]
--- examples/exported_timing_model2.tcl
+++ examples/exported_timing_model2.tcl
@@ -1,4 +1,6 @@
 read_liberty nangate45_slow.lib.gz
 read_verilog exported_timing_model.v
 link_design top
+create_clock -name clk -period 10 {clk}
+set_input_delay -clock clk 0 {in}
 write_timing_models exported_timing_model2.lib -instances [list [sta::find_instance i1] [sta::find_instance i2]]
