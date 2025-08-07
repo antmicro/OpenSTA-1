948acae69fce6e767294973b043ae2845986451c
Mateusz Gancarz <mgancarz@antmicro.com>
2025-08-07 09:26:05 +0200

    [#81462] wip: internal paths: fix writing multiple internal paths

--- liberty/LibertyWriter.cc
+++ liberty/LibertyWriter.cc
@@ -328,16 +328,18 @@ LibertyWriter::writeCell(const LibertyCell *cell)
     }
   }
 
+  fprintf(stream_, "    worst_slack_paths() {\n");
   for (const MinMax *min_max : MinMax::range()) {
     for (const RiseFall *rise_fall : RiseFall::range()) {
-      fprintf(stream_, "    worst_slack_paths() {\n");
-      fprintf(stream_, "      slack : %s;\n", time_unit_->asString(cell->getWorstSlack(), 5));
+      fprintf(stream_, "      %s_%s() {\n", min_max->to_string().c_str(), rise_fall->name());
       auto& timing_path = cell->getWorstSlackTimingPath(min_max, rise_fall);
+      fprintf(stream_, "        slack : %s;\n", time_unit_->asString(timing_path.slack, 5));
       writeTimingPath(8, timing_path.data_arrival_path);
       writeTimingPath(8, timing_path.data_required_path);
-      fprintf(stream_, "    }\n");
+      fprintf(stream_, "      }\n");
     }
   }
+  fprintf(stream_, "    }\n");
 
   fprintf(stream_, "  }\n");
   fprintf(stream_, "\n");
