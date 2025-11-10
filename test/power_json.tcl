9ea6c9cdf8c0e6ffef21ad5026da53c4d289a391
Akash Levy <akash@silimate.com>
2025-11-10 05:13:18 -0800

    Fix power_json again

--- test/power_json.tcl
+++ test/power_json.tcl
@@ -1,5 +1,5 @@
 # report_power gcd
-set sta_report_default_digits 8
+set sta_report_default_digits 6
 read_liberty ../examples/sky130hd_tt.lib.gz
 read_verilog ../examples/gcd_sky130hd.v
 link_design gcd
