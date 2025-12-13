8ac94d32745eaa9cc96617df63ea1fbf9798429c
Stan Lee <slee93@stanford.edu>
2025-12-12 16:22:34 -0800

    ok this breaks my code

--- test/generated_clock.tcl
+++ test/generated_clock.tcl
@@ -7,6 +7,8 @@ create_clock -name clk -period 10 [get_ports SYS_CLK_IN]
 # Should see two clocks clk, CLK_OUT_DIV2
 puts [get_object_name [get_clocks]]
 
+# Period of the generated clock should be 2x the period of the source clock 'clk'
 foreach_in_collection clk [get_clocks] {
   puts "[get_object_name $clk] period: [get_attribute $clk period]"
 }
+
