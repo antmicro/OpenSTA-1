efe1ef5cf0328a9c7997a2c916a97436064f4aba
Stan Lee <slee93@stanford.edu>
2025-12-15 11:38:18 -0500

    update test

--- test/generated_clock.tcl
+++ test/generated_clock.tcl
@@ -3,13 +3,9 @@ read_verilog generated_clock.v
 link_design generated_clock
 create_clock -name clk -period 10 [get_ports SYS_CLK_IN] 
 
-# Should see 3 clocks
-puts [get_object_name [get_clocks]]
+# Should see 5 clocks
+puts "Number of clocks: [ llength [get_clocks]]"
 
-# TODO figure this out
-# foreach_in_collection pin [get_ports *] {
-#   puts "[get_object_name $pin] - [get_object_name [get_property -object_type port $pin clocks]]"
-# }
 foreach_in_collection clk [get_clocks] {
   puts "[get_object_name $clk] period: [get_attribute $clk period]"
 }
--- test/generated_clock.v
+++ test/generated_clock.v
@@ -1,13 +1,17 @@
 module generated_clock (
   input wire SYS_CLK_IN,
   output wire slow_clk_int,
-  output wire fast_clk_int
+  output wire fast_clk_int,
+  output wire slow_clk_out2,
+  output wire fast_clk_out2
 );
 
   second_hierarchy u_second_hierarchy (
     .clk_in(SYS_CLK_IN),
     .slow_clk_out(slow_clk_int),
-    .fast_clk_out(fast_clk_int)
+    .fast_clk_out(fast_clk_int),
+    .slow_clk_out2(slow_clk_out2),
+    .fast_clk_out2(fast_clk_out2)
   );
 
 endmodule
@@ -15,13 +19,21 @@ endmodule
 module second_hierarchy (
   input wire clk_in,
   output wire slow_clk_out,
-  output wire fast_clk_out
+  output wire fast_clk_out,
+  output wire slow_clk_out2,
+  output wire fast_clk_out2
 );
 
   CLK_GEN clk_gen (
-    .clk_in(clk_in),
+    .CLK_IN(clk_in),
     .CLK_OUT_DIV2(slow_clk_out),
     .CLK_OUT_MUL2(fast_clk_out)
+  );
+
+  CLK_GEN clk_gen2 (
+    .CLK_IN(clk_in),
+    .CLK_OUT_DIV2(slow_clk_out2),
+    .CLK_OUT_MUL2(fast_clk_out2)
   );
 
 endmodule
