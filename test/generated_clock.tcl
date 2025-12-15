c10556c36c096e3ccddf3673f717fac90b468b4f
Stan Lee <slee93@stanford.edu>
2025-12-15 16:39:41 -0500

    tcl command not work

--- test/generated_clock.tcl
+++ test/generated_clock.tcl
@@ -10,3 +10,16 @@ puts "Number of clocks: [ llength [get_clocks]]"
 foreach_in_collection clk [get_clocks] {
   puts "[get_object_name $clk] period: [get_attribute $clk period]"
 }
+
+create_generated_clock \
+  -name clk_div4 \
+  -source [get_ports CLK_IN_1] \
+  -master_clock clk \
+  -divide_by 4 \
+  [get_pins u_second_hierarchy/clk_gen2/CLK_OUT_DIV2]
+
+# Should see 8 clocks now
+puts "Number of clocks: [ llength [get_clocks]]"
+
+# Period of this clock should be a divide by 4 overall
+puts "[get_object_name [get_clocks clk_div4]] period: [get_attribute [get_clocks clk_div4] period]"
