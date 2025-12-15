05ba743202c9b82dd74b5572d01dbf3da7e7a4b9
Stan Lee <slee93@stanford.edu>
2025-12-15 10:47:17 -0500

    can't have nested generated clocks, master clock must be propagated

--- test/generated_clock.tcl
+++ test/generated_clock.tcl
@@ -1,13 +1,15 @@
 read_liberty generated_clock.lib
-
 read_verilog generated_clock.v
 link_design generated_clock
-create_clock -name clk -period 10 [get_ports SYS_CLK_IN]
+create_clock -name clk -period 10 [get_ports SYS_CLK_IN] 
 
-# Should see two clocks clk, CLK_OUT_DIV2
+# Should see 3 clocks
 puts [get_object_name [get_clocks]]
 
-# Period of the generated clock should be 2x the period of the source clock 'clk'
+# TODO figure this out
+# foreach_in_collection pin [get_ports *] {
+#   puts "[get_object_name $pin] - [get_object_name [get_property -object_type port $pin clocks]]"
+# }
 foreach_in_collection clk [get_clocks] {
   puts "[get_object_name $clk] period: [get_attribute $clk period]"
 }
--- test/generated_clock.v
+++ test/generated_clock.v
@@ -1,35 +1,27 @@
 module generated_clock (
   input wire SYS_CLK_IN,
   output wire slow_clk_int,
-  output wire fast_clk_int,
-  output wire slow_clk_out_div4
+  output wire fast_clk_int
 );
 
   second_hierarchy u_second_hierarchy (
-    .slow_clk_int(SYS_CLK_IN),
+    .clk_in(SYS_CLK_IN),
     .slow_clk_out(slow_clk_int),
-    .fast_clk_out(fast_clk_int),
-    .slow_clk_out_div4(slow_clk_out_div4)
+    .fast_clk_out(fast_clk_int)
   );
 
 endmodule
 
 module second_hierarchy (
-  input wire slow_clk_int,
+  input wire clk_in,
   output wire slow_clk_out,
-  output wire fast_clk_out,
-  output wire slow_clk_out_div4
+  output wire fast_clk_out
 );
 
-  CLK_GEN my_divider (
-    .CLK_IN(slow_clk_int),
+  CLK_GEN clk_gen (
+    .clk_in(clk_in),
     .CLK_OUT_DIV2(slow_clk_out),
     .CLK_OUT_MUL2(fast_clk_out)
-  );
-
-  NESTED_CLKGEN divide_by_4 (
-    .CLK_IN(slow_clk_out),
-    .CLK_OUT_DIV2(slow_clk_out_div4)
   );
 
 endmodule
